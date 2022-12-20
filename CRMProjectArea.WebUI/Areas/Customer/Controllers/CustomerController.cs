9383909a9ebbb3dfa6e77ccad58e0fd116b0d43a
emreozbaskan <emreozbaskan@gmail.com>
2022-12-20 22:09:34 +0300

    role based yapı eklendi

---
+++ CRMProjectArea.WebUI/Areas/Customer/Controllers/CustomerController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace CRMProjectArea.WebUI.Areas.Customer.Controllers
+{
+	[Authorize(Roles = "Admin")]
+	[Area("customer")]
+	public class CustomerController : Controller
+	{
+		[Authorize(Roles = "CustomerList")]
+		[HttpGet("{area}/customer-list")]
+		public IActionResult CustomerList()
+		{
+			return View();
+		}
+
+		//rol based aut.
+		[Authorize(Roles = "CustomerDetail")]
+		[HttpGet("{area}/customer-detail/{id?}")]
+		public IActionResult CustomerDetail(int? id)
+		{
+			//Yeni Müteri oluturma yetkisi
+			//Müşteri silebilme yetkisi,
+			//güncelleme yetkisi
+			return View();
+		}
+
+		[AllowAnonymous]
+		[HttpGet("{area}/customer-public")]
+		public IActionResult CustomerPublic()
+		{
+			return View();
+		}
+
+	}
+}
