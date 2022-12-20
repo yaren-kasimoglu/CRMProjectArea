4ce94126ec4d6ee7d536326e1aa44f63dd224a48
emreozbaskan <emreozbaskan@gmail.com>
2022-12-20 20:55:23 +0300

    güvenlik rolleride eklendi

--- CRMProjectArea.WebUI/Areas/Admin/Controllers/UserRoleController.cs
+++ CRMProjectArea.WebUI/Areas/Admin/Controllers/UserRoleController.cs
@@ -29,16 +29,25 @@ namespace CRMProjectArea.WebUI.Areas.Admin.Controllers
 		[HttpPost("{area}/user-role-manage/{userid}")]
 		public async Task<IActionResult> UserRoleManage(int userid, List<UserRoleDTO> userRoles)
 		{
+			//kullanıcı nesnesi
+			var user = await _userManager.FindByIdAsync(userid.ToString());
+			//kullanıcının dbdeki rolleri
+			var roles = await _userManager.GetRolesAsync(user);
+			var result = await _userManager.RemoveFromRolesAsync(user, roles);
+			if (result.Succeeded)
+			{
+				var selectedRoles = userRoles.Where(t0 => t0.IsSelected == true).Select(t0 => t0.Name).ToList();
+				await _userManager.AddToRolesAsync(user, selectedRoles);
+			}
 			var userRoleList = await GetUserRoles(userid);
 			return View(userRoleList);
 		}
 
 		private async Task<List<UserRoleDTO>> GetUserRoles(int userid)
 		{
-			var user = _userManager.Users.ToList().AsEnumerable().FirstOrDefault(t0 => t0.Id == userid);
-
-			//ViewBag.userid = userid;
-			//ViewBag.userName = user.UserName;
+			var user = _userManager.Users.FirstOrDefault(t0 => t0.Id == userid);
+			ViewBag.userid = userid;
+			ViewBag.userName = user?.UserName;
 
 			var userRoleList = new List<UserRoleDTO>();
 
@@ -47,7 +56,7 @@ namespace CRMProjectArea.WebUI.Areas.Admin.Controllers
 				var userRole = new UserRoleDTO();
 				userRole.Name = item.Name;
 				userRole.Id = item.Id;
-				if (await _userManager.IsInRoleAsync(user, item.Id.ToString()))
+				if (await _userManager.IsInRoleAsync(user, item.NormalizedName))
 				{
 					userRole.IsSelected = true;
 				}
